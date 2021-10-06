c6876733ee899f5c139ee6800a6d80620b1bde6d
Aryan Sonone <72177377+Python-aryan@users.noreply.github.com>
2021-10-06 20:28:59 +0530

    Unnecessary Data Deleted
    Some Coded was Added

--- main.py
+++ main.py
@@ -1,51 +1,3 @@
-#
-# if __name__ == '__main__':
-#
-#
-#
-#     import os
-#     def soldier(path, dictionary_file, format):
-#         os.chdir(path)
-#         i = 1
-#         files = os.listdir(path)
-#
-#
-#         with open(dictionary_file) as f :
-#             filelist = f.read().split("\n")
-#
-#         for dictionary_file in files :
-#             if dictionary_file not in filelist:
-#                 os.rename(dictionary_file, files.capitalize())
-#
-#         if os.path.splitext(dictionary_file)[1] == format:
-#             os.rename((dictionary_file, f"{i}{format}"))
-#             i = i + 1
-#
-#
-# need = str(input("Please Enter the three things here each seperated by a (,) symbol\n"
-#                      "Order Should be like this : path, dictionary file, format\n"))
-#
-#
-# names = need.split(",")[0]
-# path = [0]
-# dictionary_file =[1]
-# format =[2]
-#
-# soldier(path,dictionary_file,format)
-#
-print("Your Task Was Completed")
-#
-# # print(type(names))
-#
-# print(os.getcwd())
-
-
-# Oh soldier Prettify my Folder
-
-# path, dictionary file, format
-
-# def soldier("C://", "harry.txt", "jpg")
-
 print("Hello Welcome to My Programme [Oh soldier Prettify my Folder] \n"
 	            "Please Mention the Following things \n"
             "A Directory - from where we will find the folder\n"
@@ -55,6 +7,7 @@ print("Hello Welcome to My Programme [Oh soldier Prettify my Folder] \n"
 space = input()
 
 
-soldier(r"C:\Users\Sony\OneDrive\Pictures\Screenshots",
-        r"E:\Python\Pycharm\PyCharm Community Edition 2020.2.2\Project\name12.txt", ".png" )
+soldier(r"Directory\Folder\Subfolder\Sub-sub-subfolfer",
+        r"Directory\Folder\SubFolder\SubFolder\Sub Folder\demo.txt", ".png" )
 
+print("Your Task Was Completed")
